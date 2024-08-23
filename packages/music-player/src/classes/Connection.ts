fbf530fe19e8898f4e3a448a7869d37af890fd6f
Xu Chen <79107215+xuc323@users.noreply.github.com>
2024-08-23 13:00:49 -0400

    Added event types in connection

--- packages/music-player/src/classes/Connection.ts
+++ packages/music-player/src/classes/Connection.ts
@@ -6,41 +6,35 @@ import {
   createAudioPlayer,
   createAudioResource,
   entersState,
+  joinVoiceChannel,
   NoSubscriberBehavior,
   VoiceConnection,
   VoiceConnectionStatus,
 } from "@discordjs/voice";
 import { StageChannel, VoiceChannel } from "discord.js";
 import EventEmitter from "node:events";
+import { SoundCloudStream, YouTubeStream } from "play-dl";
 import { Song } from "..";
-import { YouTubeStream } from "play-dl";
 
-export interface Connection extends EventEmitter {
-  /** Emitted when the music starts */
-  on(event: "start", listener: (resource: AudioResource<Song>) => void): this;
-
-  /** Emitted when the music starts */
-  emit(event: "start", resource: AudioResource<Song>): boolean;
-
-  /** Emitted when the music ends */
-  on(event: "end", listener: (resource: AudioResource<Song>) => void): this;
-
-  /** Emitted when the music ends */
-  emit(event: "end", resource: AudioResource<Song>): boolean;
-
-  /** Emitted when there is an error from the connection */
-  on(event: "error", listener: (error: AudioPlayerError) => void): this;
-
-  /** Emitted when there is an error from the connection */
-  emit(event: "error", error: AudioPlayerError): boolean;
+/**
+ * Events for the audio connection. This can be used to determine if a song is
+ * playing or finished
+ */
+export interface ConnectionEvents {
+  start: [resource: AudioResource<Song>];
+  end: [resource: AudioResource<Song>];
+  error: [error: AudioPlayerError];
 }
 
 export class Connection extends EventEmitter {
+  /** VoiceConnection obtained by calling {@link joinVoiceChannel} */
   private _connection?: VoiceConnection;
+  /** AudioPlayer obtained by calling {@link createAudioPlayer} */
   private _player: AudioPlayer;
+  /** Channel that the music will be played */
   private _channel: VoiceChannel | StageChannel;
+  /** AudioResource created by calling {@link createAudioResource} */
   private _resource?: AudioResource<Song>;
-  private _paused: boolean;
 
   constructor(
     connection: VoiceConnection,
@@ -50,8 +44,6 @@ export class Connection extends EventEmitter {
 
     this._connection = connection;
 
-    this._paused = false;
-
     this._player = createAudioPlayer({
       behaviors: { noSubscriber: NoSubscriberBehavior.Pause },
     });
@@ -59,6 +51,10 @@ export class Connection extends EventEmitter {
     this._channel = channel;
 
     this._connection.on("stateChange", (oldState, newState) => {
+      console.log(
+        `Connection state from ${oldState.status} to ${newState.status}.`
+      );
+      // TODO: handle more state changes
       if (newState.status === VoiceConnectionStatus.Destroyed) {
         this.stop();
         this._connection = undefined;
@@ -73,30 +69,29 @@ export class Connection extends EventEmitter {
         oldState.status !== AudioPlayerStatus.Idle &&
         newState.status === AudioPlayerStatus.Idle
       ) {
-        // the player just enters Idle state
-        if (!this._paused) {
-          this.emit("end", this._resource!);
-          this._resource = undefined;
-        }
+        // the player enters Idle state just now
+        this.emit("end", this._resource!);
+        this._resource = undefined;
       } else if (
         oldState.status !== AudioPlayerStatus.Playing &&
         newState.status === AudioPlayerStatus.Playing
       ) {
-        // the player starts playing
-        if (!this._paused) {
-          this.emit("start", this._resource!);
-        }
+        // the player enters Playing state just now
+        this.emit("start", this._resource!);
       }
     });
 
-    this._player.on("error", (error) => {
-      this.emit("error", error);
+    this._player.on("error", (err) => {
+      this.emit("error", err);
     });
 
     this._connection.subscribe(this._player);
   }
 
-  public createAudioStream(stream: YouTubeStream, metadata: Song) {
+  public createAudioStream(
+    stream: YouTubeStream | SoundCloudStream,
+    metadata: Song
+  ) {
     this._resource = createAudioResource(stream.stream, {
       inputType: stream.type,
       inlineVolume: true,
@@ -126,16 +121,6 @@ export class Connection extends EventEmitter {
     return this;
   }
 
-  public pause() {
-    this._paused = true;
-    return this._player.pause(true);
-  }
-
-  public unpause() {
-    this._paused = false;
-    return this._player.unpause();
-  }
-
   public stop() {
     return this._player.stop();
   }
@@ -145,19 +130,6 @@ export class Connection extends EventEmitter {
     if (this._connection) {
       this._connection.destroy();
     }
-  }
-
-  public get time() {
-    if (!this._resource) {
-      return 0;
-    }
-
-    return this._resource.playbackDuration;
-  }
-
-  public get volume() {
-    if (!this._resource?.volume) return 100;
-    return this._resource.volume.volume;
   }
 
   public get connection() {
@@ -176,7 +148,24 @@ export class Connection extends EventEmitter {
     return this._resource;
   }
 
-  public get paused() {
-    return this._paused;
+  public once<E extends keyof ConnectionEvents>(
+    event: E,
+    listener: (...args: ConnectionEvents[E]) => void
+  ) {
+    return super.once(event, listener);
+  }
+
+  public on<E extends keyof ConnectionEvents>(
+    event: E,
+    listener: (...args: ConnectionEvents[E]) => void
+  ) {
+    return super.on(event, listener);
+  }
+
+  public emit<E extends keyof ConnectionEvents>(
+    event: E,
+    ...args: ConnectionEvents[E]
+  ) {
+    return super.emit(event, ...args);
   }
 }
