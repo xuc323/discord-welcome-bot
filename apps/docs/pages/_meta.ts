c7c793839f6b4fad3201b4816e438d39a6a9233f
Xu Chen <79107215+xuc323@users.noreply.github.com>
2024-10-06 23:50:17 -0400

    Updated Nextra file structure

---
+++ apps/docs/pages/_meta.ts
@@ -0,0 +1,15 @@
+export default {
+  index: { title: "Introduction" },
+  host: {
+    title: "Hosting",
+    type: "page",
+  },
+  general: { title: "General Commands" },
+  music: {
+    title: "Music Commands",
+  },
+  about: {
+    title: "About",
+    type: "page",
+  },
+};
