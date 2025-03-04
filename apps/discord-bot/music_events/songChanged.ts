ad98542c9c98cc862b0203fa5d406af3165daa11
Xu Chen <79107215+xuc323@users.noreply.github.com>
2025-03-04 11:23:44 -0500

    Fix slient message

--- apps/discord-bot/music_events/songChanged.ts
+++ apps/discord-bot/music_events/songChanged.ts
@@ -1,9 +1,13 @@
 import { PlayerEventNames } from "@repo/music-player";
+import { MessageFlags } from "discord.js";
 import { PlayerEvent } from "../type";
 
 export const event: PlayerEvent<PlayerEventNames.songChanged> = {
   name: PlayerEventNames.songChanged,
   execute(client, queue, oldSong, newSong) {
-    queue.messageChannel.send(`@silent **${newSong}** is now playing.`);
+    queue.messageChannel.send({
+      content: `**${newSong}** is now playing.`,
+      flags: MessageFlags.SuppressNotifications,
+    });
   },
 };
